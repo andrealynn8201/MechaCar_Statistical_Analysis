8e384cd6a729fe82a232eddca72a0a4fb1659e78
Andrea Darrah <andreadarrah@MacBook-Air.local>
2022-06-27 17:24:48 -0500

    added deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -4,3 +4,10 @@ mechacar_df <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors =
 head(mechacar_df)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_df)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_df))
+
+## Deliverable 2
+library(tidyverse)
+suspension_coil_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+head(suspension_coil_table)
+summarize_coil_table <- suspension_coil_table %>% group_by() %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups='keep')
+summarize_coil_table_by_lot <- suspension_coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups='keep')
